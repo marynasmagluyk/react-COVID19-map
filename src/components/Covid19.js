ed86e12d45448d5380ddc5eeb65d5c16663fc04c
maryna <merry.kosiv@gmail.com>
2021-01-06 20:35:08 +0200

    add: loading component

--- src/components/Covid19.js
+++ src/components/Covid19.js
@@ -1,7 +1,9 @@
 import React, {useState, useEffect} from 'react'
+import Loading from './Loading'
+
 
 const Covid19 = () => {
-    return <div>Map</div>
+    return <Loading/>
 };
 
 export default Covid19;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//import 'bootastrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
