0f9d5322cee6a39b3c9dadb2f7ee343c5d027dfe
maryna <merry.kosiv@gmail.com>
2021-01-06 19:46:22 +0200

    add: App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 import React from 'react'
-
+import Covid19 from './components/Covid19'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Covid19/>
   );
 }
 
