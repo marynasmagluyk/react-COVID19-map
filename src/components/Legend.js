18adaf53556143bc64f182cc068b9f9224944655
maryna <merry.kosiv@gmail.com>
2021-01-07 17:44:33 +0200

    refactor: switch inline styles to styled components

---
+++ src/components/Legend.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Legend = () => {
+    return(
+        <Wrapper>
+
+        </Wrapper>
+    )
+};
+
+export default Legend;
--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,22 +1,31 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
 
 const Loading = () => {
-    return <div style={{height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-
-        <div className="spinner-border text-danger"
-             role="status">
-            <span className="sr-only">Loading...</span>
-        </div>
-        <div className="spinner-border text-info"
-             role="status">
-            <span className="sr-only">Loading...</span>
-        </div>
-        <div className="spinner-border text-success"
-             role="status">
-            <span className="sr-only">Loading...</span>
-        </div>
-    </div>
+    return (
+        <Wrapper>
+            <div className="spinner-border text-danger"
+                 role="status">
+                <span className="sr-only">Loading...</span>
+            </div>
+            <div className="spinner-border text-info"
+                 role="status">
+                <span className="sr-only">Loading...</span>
+            </div>
+            <div className="spinner-border text-success"
+                 role="status">
+                <span className="sr-only">Loading...</span>
+            </div>
+        </Wrapper>
+    )
 };
+
+const Wrapper = styled.div`
+height: 100vh;
+display: flex;
+justify-content: center; 
+align-items: center;
+`;
 
 export default Loading;
