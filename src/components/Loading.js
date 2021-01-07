3edf61162db80c3e173a3ca6db4e9f97990630fd
maryna <merry.kosiv@gmail.com>
2021-01-07 17:12:32 +0200

    styled: loading spinners

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,7 +2,21 @@ import React from 'react'
 
 
 const Loading = () => {
-    return <div>Loading...</div>
+    return <div style={{height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+
+        <div className="spinner-border text-danger"
+             role="status">
+            <span className="sr-only">Loading...</span>
+        </div>
+        <div className="spinner-border text-info"
+             role="status">
+            <span className="sr-only">Loading...</span>
+        </div>
+        <div className="spinner-border text-success"
+             role="status">
+            <span className="sr-only">Loading...</span>
+        </div>
+    </div>
 };
 
 export default Loading;
